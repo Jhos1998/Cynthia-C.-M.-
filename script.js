cdf72792798c7a59f18e087e92c70af1217584b7
Jhos1998 <118295757+Jhos1998@users.noreply.github.com>
2025-09-18 15:07:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // --- MENSAJE NORMAL ---
   const mensaje = [
-    "Hola 🤗",
+    "Hola ssssssssssssssssssssssssssssssssssssssssssssssssssss🤗",
     "Mi corazón de melón 💛",
     "Hoy, en este día tan especial de las flores amarillas,",
     "Quiero recordarte lo mucho que iluminas mi vida.",
@@ -479,3 +479,4 @@ canvas.addEventListener("touchmove", (e) => {
     createTrail(touch.clientX, touch.clientY);
   }
 }, { passive: false });
+
