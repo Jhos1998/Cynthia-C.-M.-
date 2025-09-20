0b9b27a4c90f76f7be9b5bda860e327bb702a385
Jhos1998 <118295757+Jhos1998@users.noreply.github.com>
2025-09-20 02:47:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // --- MENSAJE NORMAL ---
   const mensaje = [
-    "Holooao     🤗",
+    "Holita     🤗",
     "Mi corazón de melón 💛",
     "Hoy, en este día tan especial de las flores amarillas,",
     "Quiero recordarte lo mucho que iluminas mi vida.",
@@ -542,6 +542,7 @@ window.addEventListener("touchmove", (e) => {
     lastY = touch.clientY;
   }
 });
+
 
 
 
